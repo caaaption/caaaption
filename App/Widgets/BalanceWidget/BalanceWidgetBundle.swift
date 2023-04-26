e759742217e3c4f916a3be4d611efbf4067e65d7
tomokisun <github@tomokisun.com>
2023-04-26 09:52:55 +0900

    Create BalanceWidget

---
+++ App/Widgets/BalanceWidget/BalanceWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  BalanceWidgetBundle.swift
+//  BalanceWidget
+//
+//  Created by tomokisun on 2023/04/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BalanceWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BalanceWidget()
+    }
+}
