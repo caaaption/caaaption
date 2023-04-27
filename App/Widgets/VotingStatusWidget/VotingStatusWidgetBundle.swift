e794a8a7b3652f2b161440e1925c8c71f8586aee
tomokisun <github@tomokisun.com>
2023-04-28 03:41:55 +0900

    Add voting status widget

---
+++ App/Widgets/VotingStatusWidget/VotingStatusWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  VotingStatusWidgetBundle.swift
+//  VotingStatusWidget
+//
+//  Created by tomokisun on 2023/04/28.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct VotingStatusWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        VotingStatusWidget()
+    }
+}
