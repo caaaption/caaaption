e69e7f2197f14f053fd52ba19c424c4e2adfa06b
tomokisun <github@tomokisun.com>
2023-04-28 02:10:25 +0900

    Add LiveKey

--- Sources/SnapshotClient/Client.swift
+++ Sources/SnapshotClient/Client.swift
@@ -1,5 +1,6 @@
 import SnapshotGraphQLModel
+import Apollo
 
 public struct SnapshotClient {
-  public var proposal: @Sendable (String) async throws -> SnapshotGraphQLModel.ProposalQuery.Data.Proposal
+  public var proposal: @Sendable (String) async throws -> GraphQLResult<SnapshotGraphQLModel.ProposalQuery.Data>
 }
--- Sources/SnapshotClient/LiveKey.swift
+++ Sources/SnapshotClient/LiveKey.swift
@@ -1,8 +1,35 @@
-//
-//  File.swift
-//  
-//
-//  Created by tomokisun on 2023/04/28.
-//
-
+import Dependencies
+import Apollo
+import ApolloAPI
+import SnapshotGraphQLModel
 import Foundation
+
+extension SnapshotClient: DependencyKey {
+  public static let liveValue = Self.live()
+  
+  public static func live() -> Self {
+    
+    let url = URL(string: "https://hub.snapshot.org/graphql")!
+    let apolloClient = ApolloClient(url: url)
+    let actor = SnapshotClientActor(apolloClient: apolloClient)
+    
+    return Self(
+      proposal: { try await actor.proposal(id: $0) }
+    )
+  }
+}
+
+actor SnapshotClientActor {
+  let apolloClient: ApolloClient
+  
+  init(apolloClient: ApolloClient) {
+    self.apolloClient = apolloClient
+  }
+  
+  func proposal(
+    id: String
+  ) async throws -> GraphQLResult<SnapshotGraphQLModel.ProposalQuery.Data> {
+    let query = SnapshotGraphQLModel.ProposalQuery(id: id)
+    return try await apolloClient.fetch(query: query)
+  }
+}
