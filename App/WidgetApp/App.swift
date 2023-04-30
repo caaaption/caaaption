ed3718fa5ce4b512d4a093aeed1a447c3017df76
tomokisun <github@tomokisun.com>
2023-05-01 00:57:16 +0900

    Create WidgetApp

---
+++ App/WidgetApp/App.swift
@@ -0,0 +1,48 @@
+import AppFeature
+import ComposableArchitecture
+import SwiftUI
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+  let store = Store(
+    initialState: AppReducer.State(),
+    reducer: AppReducer().transformDependency(\.self) { _ in }
+  )
+
+  var viewStore: ViewStore<Void, AppReducer.Action> {
+    return ViewStore(store.stateless)
+  }
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    viewStore.send(.appDelegate(.didFinishLaunching))
+
+    return true
+  }
+
+  func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    viewStore.send(.appDelegate(.didRegisterForRemoteNotifications(.success(deviceToken))))
+  }
+
+  func application(
+    _ application: UIApplication,
+    didFailToRegisterForRemoteNotificationsWithError error: Error
+  ) {
+    viewStore.send(.appDelegate(.didRegisterForRemoteNotifications(.failure(error))))
+  }
+}
+
+@main
+struct CaaaptionApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+  var body: some Scene {
+    WindowGroup {
+      AppView(store: appDelegate.store)
+    }
+  }
+}
