8d23bafefc6906d9ad824df089be65fa3cb8ecca
tomokisun <github@tomokisun.com>
2023-05-01 05:51:47 +0900

    refreshable sleep 2 seconds

--- caaaption/Sources/WidgetSearchFeature/WidgetSearchReducer.swift
+++ caaaption/Sources/WidgetSearchFeature/WidgetSearchReducer.swift
@@ -13,8 +13,28 @@ public struct WidgetSearchReducer: ReducerProtocol {
     case refreshable
     case binding(BindingAction<State>)
   }
+  
+  @Dependency(\.mainQueue) var mainQueue
+  private enum WidgetRequestID {}
 
   public var body: some ReducerProtocol<State, Action> {
     BindingReducer()
+    Reduce { state, action in
+      switch action {
+      case .task:
+        return EffectTask.none
+        
+      case .refreshable:
+        return EffectTask.task {
+          try await self.mainQueue.sleep(for: .seconds(2))
+          return Action.task
+        }
+        .animation()
+        .cancellable(id: WidgetRequestID.self)
+        
+      case .binding:
+        return EffectTask.none
+      }
+    }
   }
 }
