d007e7a22517a4564dbe7c46fc334cbf6719bbf8
tomokisun <github@tomokisun.com>
2023-05-01 01:16:51 +0900

    Setup widgets

--- .github/workflows/scripts/auto-update-version.sh
+++ .github/workflows/scripts/auto-update-version.sh
@@ -2,4 +2,6 @@
 
 NEW_VERSION=$(date '+%y').$(date '+%m').$(date '+%d')
 CURRENT_VERSION=$(defaults read $(pwd)/App/iOS/Info CFBundleShortVersionString)
+
 sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" App/iOS/Info.plist
+sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" App/WidgetApp/Info.plist
