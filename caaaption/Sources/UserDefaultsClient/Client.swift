073273b96ef8a5173889910a738ea2c15649e432
tomokisun <github@tomokisun.com>
2023-05-02 20:27:33 +0900

    Add UserDefaults

--- caaaption/Sources/UserDefaultsClient/Client.swift
+++ caaaption/Sources/UserDefaultsClient/Client.swift
@@ -1,8 +1,13 @@
-//
-//  File.swift
-//  
-//
-//  Created by tomokisun on 2023/05/02.
-//
-
 import Foundation
+
+public struct UserDefaultsClient {
+  public var boolForKey: @Sendable (String) -> Bool
+  public var dataForKey: @Sendable (String) -> Data?
+  public var doubleForKey: @Sendable (String) -> Double
+  public var integerForKey: @Sendable (String) -> Int
+  public var remove: @Sendable (String) async -> Void
+  public var setBool: @Sendable (Bool, String) async -> Void
+  public var setData: @Sendable (Data?, String) async -> Void
+  public var setDouble: @Sendable (Double, String) async -> Void
+  public var setInteger: @Sendable (Int, String) async -> Void
+}
--- caaaption/Sources/UserDefaultsClient/TestKey.swift
+++ caaaption/Sources/UserDefaultsClient/TestKey.swift
@@ -1,8 +1,40 @@
-//
-//  File.swift
-//  
-//
-//  Created by tomokisun on 2023/05/02.
-//
-
+import Dependencies
 import Foundation
+import XCTestDynamicOverlay
+
+extension DependencyValues {
+  public var userDefaults: UserDefaultsClient {
+    get { self[UserDefaultsClient.self] }
+    set { self[UserDefaultsClient.self] = newValue }
+  }
+}
+
+extension UserDefaultsClient: TestDependencyKey {
+  public static let previewValue = Self.noop
+
+  public static let testValue = Self(
+    boolForKey: unimplemented("\(Self.self).boolForKey", placeholder: false),
+    dataForKey: unimplemented("\(Self.self).dataForKey", placeholder: nil),
+    doubleForKey: unimplemented("\(Self.self).doubleForKey", placeholder: 0),
+    integerForKey: unimplemented("\(Self.self).integerForKey", placeholder: 0),
+    remove: unimplemented("\(Self.self).remove"),
+    setBool: unimplemented("\(Self.self).setBool"),
+    setData: unimplemented("\(Self.self).setData"),
+    setDouble: unimplemented("\(Self.self).setDouble"),
+    setInteger: unimplemented("\(Self.self).setInteger")
+  )
+}
+
+extension UserDefaultsClient {
+  public static let noop = Self(
+    boolForKey: { _ in false },
+    dataForKey: { _ in nil },
+    doubleForKey: { _ in 0 },
+    integerForKey: { _ in 0 },
+    remove: { _ in },
+    setBool: { _, _ in },
+    setData: { _, _ in },
+    setDouble: { _, _ in },
+    setInteger: { _, _ in }
+  )
+}
