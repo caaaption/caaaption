32b511f8bf28fda2aa80910ca74b04642f4e8ff3
tomokisun <github@tomokisun.com>
2023-05-09 17:57:14 +0900

    Added confirmation dialog to proposals screen (#95)

--- caaaption/Sources/VoteWidgetFeature/ProposalsReducer.swift
+++ caaaption/Sources/VoteWidgetFeature/ProposalsReducer.swift
@@ -2,22 +2,62 @@ import ApolloHelpers
 import ComposableArchitecture
 import SnapshotModel
 
+public typealias Proposal = WrappedIdentifiable<SnapshotModel.ProposalCardFragment>
+
 public struct ProposalsReducer: ReducerProtocol {
   public init() {}
 
   public struct State: Equatable {
-    public var proposals: IdentifiedArrayOf<WrappedIdentifiable<SnapshotModel.ProposalCardFragment>>
+    public var proposals: IdentifiedArrayOf<Proposal>
+    @PresentationState var dialog: ConfirmationDialogState<Action.Dialog>?
 
-    public init(proposals: IdentifiedArrayOf<WrappedIdentifiable<SnapshotModel.ProposalCardFragment>>) {
+    public init(
+      proposals: IdentifiedArrayOf<Proposal>
+    ) {
       self.proposals = proposals
     }
   }
 
-  public enum Action: Equatable {}
+  public enum Action: Equatable {
+    case dialog(PresentationAction<Dialog>)
+    case tappedProposal(Proposal)
+
+    public enum Dialog {
+      case confirmDiscard
+      case confirmAddWidget
+    }
+  }
 
   public var body: some ReducerProtocol<State, Action> {
-    Reduce { _, action in
-      switch action {}
+    Reduce { state, action in
+      switch action {
+      case .dialog(.presented(.confirmAddWidget)):
+        print("confirm add widget")
+        return EffectTask.none
+        
+      case .dialog:
+        return EffectTask.none
+        
+      case let .tappedProposal(proposal):
+        state.dialog = ConfirmationDialogState(titleVisibility: .visible) {
+          TextState("Display in Widget.")
+        } actions: {
+          ButtonState(action: .confirmAddWidget) {
+            TextState("Add widget")
+          }
+          ButtonState(role: .cancel, action: .confirmDiscard) {
+            TextState("Cancel")
+          }
+        } message: {
+          TextState(
+            """
+            "\(proposal.value.title)" in the Widget.
+            """
+          )
+        }
+        return EffectTask.none
+      }
     }
+    .ifLet(\.$dialog, action: /Action.dialog)
   }
 }
