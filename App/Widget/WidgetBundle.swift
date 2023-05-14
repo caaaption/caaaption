3959a60d81378432b2988c115868415308460279
tomokisun <github@tomokisun.com>
2023-05-14 19:08:08 +0900

    Update widget extension

--- App/Widget/WidgetBundle.swift
+++ App/Widget/WidgetBundle.swift
@@ -3,16 +3,24 @@ import Dependencies
 import QuickNodeClient
 import SwiftUI
 import UserDefaultsClient
+import VoteWidget
 import WidgetKit
+import SnapshotClient
 
 @main
-struct BalanceWidgetBundle: WidgetBundle {
+struct MainWidgetBundle: WidgetBundle {
   var body: some Widget {
     withDependencies {
       $0.userDefaults = .liveValue
       $0.quickNodeClient = .liveValue
     } operation: {
       BalanceWidget.Entrypoint()
+    }
+    
+    withDependencies {
+      $0.snapshotClient = .liveValue
+    } operation: {
+      VoteWidget.Entrypoint()
     }
   }
 }
