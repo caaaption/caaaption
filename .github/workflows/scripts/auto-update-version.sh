5810df8d0eec5cc1fa32522d4f111d4be4635862
tomokisun <github@tomokisun.com>
2023-05-15 16:25:51 +0900

    Info plist (#104)

--- .github/workflows/scripts/auto-update-version.sh
+++ .github/workflows/scripts/auto-update-version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NEW_VERSION=$(date '+%y').$(date '+%m').$(date '+%d')
-CURRENT_VERSION=$(defaults read $(pwd)/App/WidgetApp/Info CFBundleShortVersionString)
+CURRENT_VERSION=$(defaults read $(pwd)/App/iOS/Info CFBundleShortVersionString)
 
-sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" App/WidgetApp/Info.plist
+sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" App/iOS/Info.plist
+sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" App/WidgetExtension/Info.plist
