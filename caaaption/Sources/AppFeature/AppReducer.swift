67a2f91662a768e444accb3d6fa712992f59433d
tomokisun <github@tomokisun.com>
2023-05-16 19:43:29 +0900

    Add onOpenURL

--- caaaption/Sources/AppFeature/AppReducer.swift
+++ caaaption/Sources/AppFeature/AppReducer.swift
@@ -1,6 +1,7 @@
 import ComposableArchitecture
 import SwiftUI
 import WidgetSearchFeature
+import UIApplicationClient
 
 public struct AppReducer: ReducerProtocol {
   public init() {}
@@ -14,11 +15,29 @@ public struct AppReducer: ReducerProtocol {
   public enum Action: Equatable {
     case appDelegate(AppDelegateReducer.Action)
     case search(WidgetSearchReducer.Action)
+    
+    case onOpenURL(URL)
   }
+  
+  @Dependency(\.applicationClient.open) var openURL
 
   public var body: some ReducerProtocol<State, Action> {
     Scope(state: \.search, action: /Action.search) {
       WidgetSearchReducer()
+    }
+    Reduce { _, action in
+      switch action {
+      case .appDelegate:
+        return .none
+
+      case .search:
+        return .none
+
+      case let .onOpenURL(url):
+        return .run { _ in
+          _ = await self.openURL(url, [:])
+        }
+      }
     }
   }
 }
@@ -31,8 +50,11 @@ public struct AppView: View {
   }
 
   public var body: some View {
-    NavigationStack {
-      WidgetSearchView(store: store.scope(state: \.search, action: AppReducer.Action.search))
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      NavigationStack {
+        WidgetSearchView(store: store.scope(state: \.search, action: AppReducer.Action.search))
+      }
+      .onOpenURL(perform: { viewStore.send(.onOpenURL($0)) })
     }
   }
 }
