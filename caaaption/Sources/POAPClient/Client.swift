0f6f7fc86aecb3514a007522a7ee24d35b620bba
tomokisun <github@tomokisun.com>
2023-05-19 14:47:24 +0900

    Add client to poap

--- caaaption/Sources/POAPClient/Client.swift
+++ caaaption/Sources/POAPClient/Client.swift
@@ -1,8 +1,25 @@
-//
-//  File.swift
-//  
-//
-//  Created by tomokisun on 2023/05/19.
-//
+public struct POAPClient {
+  public var scan: @Sendable () async throws -> String
+}
 
-import Foundation
+extension POAPClient {
+  public struct Scan: Codable, Equatable, Identifiable {
+    public var id: String {
+      return tokenId
+    }
+    public let tokenId: String
+    public let owner: String
+    public let chain: String
+    public let created: String
+    public let event: Event
+    
+    public struct Event: Codable, Equatable, Identifiable {
+      public let id: Int
+      public let fancyId: String
+      public let name: String
+      public let eventUrl: String
+      public let imageUrl: String
+      public let description: String
+    }
+  }
+}
