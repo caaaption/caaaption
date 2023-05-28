13e3df99bd3bf1ab0157c0eeef7f8405e0033d78
tomokisun <github@tomokisun.com>
2023-05-29 05:03:50 +0900

    Deleted Kingflisher (#131)

--- caaaption/Package.swift
+++ caaaption/Package.swift
@@ -13,7 +13,6 @@ var package = Package(
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "navigation-beta"),
     .package(url: "https://github.com/apollographql/apollo-ios", from: "1.1.2"),
-    .package(url: "https://github.com/onevcat/Kingfisher", from: "7.7.0"),
     .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI", from: "2.2.3"),
   ]
 )
