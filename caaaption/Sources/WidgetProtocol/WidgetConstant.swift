f43b45483d971495ce7ab700c80d27eb4e5ef0dd
tomokisun <github@tomokisun.com>
2023-06-07 13:28:44 +0900

    Update widget protocol (#190)

--- caaaption/Sources/WidgetProtocol/WidgetConstant.swift
+++
@@ -1,8 +0,0 @@
-import WidgetKit
-
-public protocol WidgetConstant {
-  static var displayName: String { get }
-  static var description: String { get }
-  static var kind: String { get }
-  static var supportedFamilies: [WidgetFamily] { get }
-}
