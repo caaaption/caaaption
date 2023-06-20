a4a9a244eb9f4241b2785c752adef25229deb080
tomokisun <github@tomokisun.com>
2023-06-21 03:39:12 +0900

    Add HTTPMethod, InternalError and Request to APIKit

--- Packages/ClientPackage/Sources/APIKit/APIKit.swift
+++ Packages/ClientPackage/Sources/APIKit/APIKit.swift
@@ -1,46 +1,5 @@
 import Foundation
 
-public enum HTTPMethod: String {
-  case get = "GET"
-  case post = "POST"
-}
-
-public enum InternalError: Error {
-  case unknown
-  case service(Decodable)
-}
-
-public protocol Request {
-  var baseURL: URL { get }
-  var path: String { get }
-  var method: HTTPMethod { get }
-  var queryItems: [URLQueryItem] { get }
-  var httpBody: Data? { get }
-  var headerFields: [String: String] { get }
-  associatedtype Response: Decodable
-  associatedtype Error: Decodable
-}
-
-public extension Request {
-  var queryItems: [URLQueryItem] { [] }
-  var httpBody: Data? { nil }
-  var headerFields: [String: String] { [:] }
-}
-
-extension Request {
-  var urlRequest: URLRequest {
-    let url = baseURL.appendingPathComponent(path)
-    var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-    urlComponents.queryItems = queryItems
-    var request = URLRequest(url: urlComponents.url!)
-    request.allHTTPHeaderFields = self.headerFields
-    request.httpMethod = method.rawValue
-    request.httpBody = httpBody
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    return request
-  }
-}
-
 public class APIKit {
   let session = URLSession.shared
   public static let shared = APIKit()
