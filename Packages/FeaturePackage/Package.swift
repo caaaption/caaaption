c27cf6f557008175290f6cd44c4e479196baa483
tomokisun <github@tomokisun.com>
2023-06-28 09:48:03 +0900

    Add feature libraries and targets

--- Packages/FeaturePackage/Package.swift
+++ Packages/FeaturePackage/Package.swift
@@ -10,6 +10,19 @@ var package = Package(
     .iOS(.v16),
     .macOS(.v13),
   ],
+  products: [
+    .library(name: "AppFeature", targets: ["AppFeature"]),
+    .library(name: "WidgetSearchFeature", targets: ["WidgetSearchFeature"]),
+    .library(name: "AccountFeature", targets: ["AccountFeature"]),
+    .library(name: "ContributorFeature", targets: ["ContributorFeature"]),
+    .library(name: "BalanceWidgetFeature", targets: ["BalanceWidgetFeature"]),
+    .library(name: "VoteWidgetFeature", targets: ["VoteWidgetFeature"]),
+    .library(name: "OnboardFeature", targets: ["OnboardFeature"]),
+    .library(name: "POAPWidgetFeature", targets: ["POAPWidgetFeature"]),
+    .library(name: "GasPriceWidgetFeature", targets: ["GasPriceWidgetFeature"]),
+    .library(name: "WidgetTabFeature", targets: ["WidgetTabFeature"]),
+    .library(name: "LinkFeature", targets: ["LinkFeature"]),
+  ],
   dependencies: [
     .package(path: "../HelperPackage"),
     .package(path: "../WidgetPackage"),
@@ -18,81 +31,66 @@ var package = Package(
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.54.0"),
     .package(url: "https://github.com/apollographql/apollo-ios", from: "1.2.2"),
     .package(url: "https://github.com/onevcat/Kingfisher", from: "7.8.1"),
+  ],
+  targets: [
+    .target(name: "AppFeature", dependencies: [
+      "WidgetTabFeature",
+      "OnboardFeature",
+    ]),
+    .target(name: "WidgetSearchFeature", dependencies: [
+      "VoteWidgetFeature",
+      "POAPWidgetFeature",
+      "BalanceWidgetFeature",
+      "GasPriceWidgetFeature",
+      .product(name: "AnalyticsReducer", package: "HelperPackage"),
+    ]),
+    .target(name: "AccountFeature", dependencies: [
+      "LinkFeature",
+      "ContributorFeature",
+    ]),
+    .target(name: "ContributorFeature", dependencies: [
+      .product(name: "GitHubClient", package: "ClientPackage"),
+      .product(name: "SwiftUIHelpers", package: "HelperPackage"),
+      .product(name: "PlaceholderAsyncImage", package: "HelperPackage"),
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
+    .target(name: "BalanceWidgetFeature", dependencies: [
+      .product(name: "SwiftUIHelpers", package: "HelperPackage"),
+      .product(name: "WidgetClient", package: "ClientPackage"),
+      .product(name: "BalanceWidget", package: "WidgetPackage"),
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
+    .target(name: "VoteWidgetFeature", dependencies: [
+      .product(name: "SwiftUIHelpers", package: "HelperPackage"),
+      .product(name: "VoteWidget", package: "WidgetPackage"),
+      .product(name: "WidgetClient", package: "ClientPackage"),
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
+    .target(name: "OnboardFeature", dependencies: [
+      .product(name: "SwiftUIHelpers", package: "HelperPackage"),
+      .product(name: "AuthClient", package: "ClientPackage"),
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
+    .target(name: "POAPWidgetFeature", dependencies: [
+      .product(name: "SwiftUIHelpers", package: "HelperPackage"),
+      .product(name: "Kingfisher", package: "Kingfisher"),
+      .product(name: "POAPWidget", package: "WidgetPackage"),
+      .product(name: "POAPClient", package: "ClientPackage"),
+      .product(name: "WidgetClient", package: "ClientPackage"),
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
+    .target(name: "GasPriceWidgetFeature", dependencies: [
+      .product(name: "SwiftUIHelpers", package: "HelperPackage"),
+      .product(name: "GasPriceWidget", package: "WidgetPackage"),
+      .product(name: "WidgetClient", package: "ClientPackage"),
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
+    .target(name: "WidgetTabFeature", dependencies: [
+      "WidgetSearchFeature",
+      "AccountFeature",
+    ]),
+    .target(name: "LinkFeature", dependencies: [
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
   ]
 )
-
-// Features
-package.products.append(contentsOf: [
-  .library(name: "AppFeature", targets: ["AppFeature"]),
-  .library(name: "WidgetSearchFeature", targets: ["WidgetSearchFeature"]),
-  .library(name: "AccountFeature", targets: ["AccountFeature"]),
-  .library(name: "ContributorFeature", targets: ["ContributorFeature"]),
-  .library(name: "BalanceWidgetFeature", targets: ["BalanceWidgetFeature"]),
-  .library(name: "VoteWidgetFeature", targets: ["VoteWidgetFeature"]),
-  .library(name: "OnboardFeature", targets: ["OnboardFeature"]),
-  .library(name: "POAPWidgetFeature", targets: ["POAPWidgetFeature"]),
-  .library(name: "GasPriceWidgetFeature", targets: ["GasPriceWidgetFeature"]),
-  .library(name: "WidgetTabFeature", targets: ["WidgetTabFeature"]),
-  .library(name: "LinkFeature", targets: ["LinkFeature"]),
-])
-package.targets.append(contentsOf: [
-  .target(name: "AppFeature", dependencies: [
-    "WidgetTabFeature",
-    "OnboardFeature",
-  ]),
-  .target(name: "WidgetSearchFeature", dependencies: [
-    "VoteWidgetFeature",
-    "POAPWidgetFeature",
-    "BalanceWidgetFeature",
-    "GasPriceWidgetFeature",
-    .product(name: "AnalyticsReducer", package: "HelperPackage"),
-  ]),
-  .target(name: "AccountFeature", dependencies: [
-    "LinkFeature",
-    "ContributorFeature",
-  ]),
-  .target(name: "ContributorFeature", dependencies: [
-    .product(name: "GitHubClient", package: "ClientPackage"),
-    .product(name: "SwiftUIHelpers", package: "HelperPackage"),
-    .product(name: "PlaceholderAsyncImage", package: "HelperPackage"),
-    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  ]),
-  .target(name: "BalanceWidgetFeature", dependencies: [
-    .product(name: "SwiftUIHelpers", package: "HelperPackage"),
-    .product(name: "WidgetClient", package: "ClientPackage"),
-    .product(name: "BalanceWidget", package: "WidgetPackage"),
-    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  ]),
-  .target(name: "VoteWidgetFeature", dependencies: [
-    .product(name: "SwiftUIHelpers", package: "HelperPackage"),
-    .product(name: "VoteWidget", package: "WidgetPackage"),
-    .product(name: "WidgetClient", package: "ClientPackage"),
-    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  ]),
-  .target(name: "OnboardFeature", dependencies: [
-    .product(name: "SwiftUIHelpers", package: "HelperPackage"),
-    .product(name: "AuthClient", package: "ClientPackage"),
-    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  ]),
-  .target(name: "POAPWidgetFeature", dependencies: [
-    .product(name: "SwiftUIHelpers", package: "HelperPackage"),
-    .product(name: "Kingfisher", package: "Kingfisher"),
-    .product(name: "POAPWidget", package: "WidgetPackage"),
-    .product(name: "POAPClient", package: "ClientPackage"),
-    .product(name: "WidgetClient", package: "ClientPackage"),
-    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  ]),
-  .target(name: "GasPriceWidgetFeature", dependencies: [
-    .product(name: "SwiftUIHelpers", package: "HelperPackage"),
-    .product(name: "GasPriceWidget", package: "WidgetPackage"),
-    .product(name: "WidgetClient", package: "ClientPackage"),
-    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  ]),
-  .target(name: "WidgetTabFeature", dependencies: [
-    "WidgetSearchFeature",
-    "AccountFeature",
-  ]),
-  .target(name: "LinkFeature", dependencies: [
-    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  ]),
-])
