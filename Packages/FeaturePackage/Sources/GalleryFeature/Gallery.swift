95d657b292f6bd9f42e03cf00858a43ab8098882
tomokisun <github@tomokisun.com>
2023-07-06 08:10:14 +0900

    Create GalleryFeature (#359)

---
+++ Packages/FeaturePackage/Sources/GalleryFeature/Gallery.swift
@@ -0,0 +1,96 @@
+import ComposableArchitecture
+import SwiftUI
+
+public struct GalleryReducer: ReducerProtocol {
+  public init() {}
+
+  public struct State: Equatable {
+    public init() {}
+  }
+
+  public enum Action: Equatable {
+    case onTask
+  }
+
+  public var body: some ReducerProtocol<State, Action> {
+    Reduce { _, action in
+      switch action {
+        case .onTask:
+          return .none
+      }
+    }
+  }
+}
+
+public struct GalleryView: View {
+  let store: StoreOf<GalleryReducer>
+
+  public init(store: StoreOf<GalleryReducer>) {
+    self.store = store
+  }
+
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      ScrollView(.vertical, showsIndicators: true) {
+        LazyVStack(spacing: 16) {
+          GallerySection(
+            title: "Get Stuff Done",
+            description: "Shortcuts to help you focus",
+            action: {}
+          ) {
+            ForEach(0..<10, id: \.self) { _ in
+              Color.blue
+                .frame(width: 170, height: 170)
+                .cornerRadius(12)
+            }
+          }
+          
+          Divider()
+          
+          GallerySection(
+            title: "Quick Shortcuts",
+            description: "Less taps, more done with these fast shortcuts!",
+            action: {}
+          ) {
+            ForEach(0..<10, id: \.self) { _ in
+              Color.red
+                .frame(width: 170, height: 170)
+                .cornerRadius(12)
+            }
+          }
+          
+          Divider()
+          
+          GallerySection(
+            title: "Use Your Clipboard",
+            description: "Copy and paste the day away.",
+            action: {}
+          ) {
+            ForEach(0..<10, id: \.self) { _ in
+              Color.gray
+                .frame(width: 170, height: 170)
+                .cornerRadius(12)
+            }
+          }
+        }
+      }
+      .navigationTitle("Gallery")
+      .task { await viewStore.send(.onTask).finish() }
+    }
+  }
+}
+
+#if DEBUG
+  struct GalleryViewPreviews: PreviewProvider {
+    static var previews: some View {
+      NavigationStack {
+        GalleryView(
+          store: .init(
+            initialState: GalleryReducer.State(),
+            reducer: GalleryReducer()
+          )
+        )
+      }
+    }
+  }
+#endif
---
+++ Packages/FeaturePackage/Sources/GalleryFeature/GallerySection.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct GallerySection<Content: View>: View {
+  let title: LocalizedStringKey
+  let description: LocalizedStringKey
+  let action: () -> Void
+  let content: () -> Content
+  
+  init(
+    title: LocalizedStringKey,
+    description: LocalizedStringKey,
+    action: @escaping () -> Void,
+    @ViewBuilder content: @escaping () -> Content
+  ) {
+    self.title = title
+    self.description = description
+    self.action = action
+    self.content = content
+  }
+  
+  var body: some View {
+    VStack(spacing: 16) {
+      HStack(alignment: .top) {
+        VStack(alignment: .leading, spacing: 4) {
+          Text(title)
+            .font(.title3)
+            .bold()
+          Text(description)
+            .foregroundColor(.secondary)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        Button("See All", action: action)
+      }
+      .padding(.horizontal, 16)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        LazyHStack {
+          content()
+        }
+        .padding(.horizontal, 16)
+      }
+    }
+  }
+}
+
+#if DEBUG
+  struct GallerySectionPreviews: PreviewProvider {
+    static var previews: some View {
+      GallerySection(
+        title: "Get Stuff Done",
+        description: "Shortcuts to help you focus",
+        action: {}
+      ) {
+        Text("Content")
+      }
+      .frame(width: 375)
+      .previewLayout(.sizeThatFits)
+    }
+  }
+#endif
+
