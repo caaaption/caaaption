471dc73eda197c0087881a635bcd2f9386678491
tomokisun <github@tomokisun.com>
2023-07-11 05:37:23 +0900

    Remove unused imports and update widget entrypoints

--- App/WidgetExtension/WidgetExtensionBundle.swift
+++ App/WidgetExtension/WidgetExtensionBundle.swift
@@ -1,36 +1,15 @@
 import BalanceWidget
 import Dependencies
-import MirrorWidget
 import POAPWidget
-import QuickNodeClient
-import SnapshotClient
-import SnapshotSpaceWidget
 import SwiftUI
-import UserDefaultsClient
 import VoteWidget
 import WidgetKit
 
 @main
 struct WidgetExtensionBundle: WidgetBundle {
   var body: some Widget {
-    withDependencies {
-      $0.quickNodeClient = .liveValue
-      $0.userDefaults = .liveValue
-    } operation: {
-      BalanceWidget.Entrypoint()
-    }
-
-    withDependencies {
-      $0.snapshotClient = .liveValue
-      $0.userDefaults = .liveValue
-    } operation: {
-      VoteWidget.Entrypoint()
-    }
-
-    withDependencies {
-      $0.userDefaults = .liveValue
-    } operation: {
-      POAPWidget.Entrypoint()
-    }
+    BalanceWidget.Entrypoint()
+    VoteWidget.Entrypoint()
+    POAPWidget.Entrypoint()
   }
 }
