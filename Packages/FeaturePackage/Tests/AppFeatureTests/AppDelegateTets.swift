646dfa8b54abf5bbf565b98f5f9dd8adc8a5e7f3
tomokisun <github@tomokisun.com>
2023-07-13 12:05:37 +0900

    Add AppFeatureTests target and test cases for AppDelegate

---
+++ Packages/FeaturePackage/Tests/AppFeatureTests/AppDelegateTets.swift
@@ -0,0 +1,39 @@
+import ComposableArchitecture
+import XCTest
+
+@testable import AppFeature
+
+@MainActor
+class AppDelegateTests: XCTestCase {
+  func testAnalyticsEnabledForStaging() async {
+    let store = TestStore(
+      initialState: AppDelegateReducer.State(),
+      reducer: AppDelegateReducer()
+    )
+    
+    store.dependencies.build.bundleIdentifier = { "com.caaaption-staging" }
+    store.dependencies.firebaseCore.configure = {}
+    store.dependencies.analytics.setAnalyticsCollectionEnabled = { value in
+      XCTAssertTrue(value)
+    }
+    
+    let task = await store.send(.didFinishLaunching)
+    await task.cancel()
+  }
+  
+  func testAnalyticsDisabledForProduction() async {
+    let store = TestStore(
+      initialState: AppDelegateReducer.State(),
+      reducer: AppDelegateReducer()
+    )
+    
+    store.dependencies.build.bundleIdentifier = { "com.caaaption" }
+    store.dependencies.firebaseCore.configure = {}
+    store.dependencies.analytics.setAnalyticsCollectionEnabled = { value in
+      XCTAssertFalse(value)
+    }
+    
+    let task = await store.send(.didFinishLaunching)
+    await task.cancel()
+  }
+}
